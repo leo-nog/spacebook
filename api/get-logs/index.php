7d63339e8e060306437b5f0fedfe48dc8dc1580b
Léo Nogueira <82295691+leo-nog@users.noreply.github.com>
2021-10-03 19:33:25 -0300

    Fix get logs

--- api/get-logs/index.php
+++ api/get-logs/index.php
@@ -25,9 +25,10 @@ if(empty($_POST['user_id'])) {
 $loop = 0;
 
 while($log = mysqli_fetch_array($query)) {
-    $user = mysqli_fetch_assoc(mysqli_query($mysqli, "SELECT `name` FROM users WHERE id = {$log['user_id']}"));
+    $user = mysqli_fetch_assoc(mysqli_query($mysqli, "SELECT `id`, `name` FROM users WHERE id = {$log['user_id']}"));
 
     $result['logs'][$loop]['date_log'] = $log['date_log'];
+    $result['logs'][$loop]['user_id']  = $user['id'];
     $result['logs'][$loop]['username'] = $user['name'];
     $result['logs'][$loop]['content']  = $log['content'];
 
