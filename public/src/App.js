a12be323bdae0c8a0db5ded6e24b81a51ae1686d
Daniel <daniel.alex6435@outlook.com>
2021-10-09 00:27:47 -0300

    implementação css + pages
    algumas mudanças no css do app.js
    criado page inicio e css inicio

--- public/src/App.js
+++ public/src/App.js
@@ -1,14 +1,13 @@
 import React from 'react'
-import style from './App.css';
+import './App.css';
 
 function App()  {
   return (
     <div className="App">
-      <div className="imagelogin" style={style}>
+      <div className="Imagelogin" >
         <img src="https://i.ibb.co/wBm3x3Y/Adobe-Stock-277977555.png" alt="Astronalta em um Planeta" />
-        
-      </div>
-      <div>
+        </div>
+      <div className="Login">
         <div className="logotipo">
           <img src="https://i.ibb.co/NVqpRyV/logo-Spacebook.png" alt= "Logotipo Spacebook" />
         </div>
@@ -18,11 +17,13 @@ function App()  {
         <div className="Titleusername">
           <p>Username</p>
         </div>
-        <div className="logininputemail" style={style}>
-          <input
-            type="text"
-            placeholder="E-mail">   
+        <div className="logininputemail" >
+          <div className="Retangle">
+          <input 
+          type="text" 
+          placeholder="E-mail">   
           </input>
+          </div>
         </div>
         <div className="Titlepass">
           <p>Password</p>
